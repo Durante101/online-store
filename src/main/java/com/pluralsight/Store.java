7f2f397497a83f2d5be7934eecf0d29a0ff982cf
Durante101 <durthornton@gmail.com>
2025-05-08 04:11:48 -0400

    Used iterator in order to safely remove product from list

--- src/main/java/com/pluralsight/Store.java
+++ src/main/java/com/pluralsight/Store.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Store {
@@ -37,6 +38,7 @@ public class Store {
                     break;
                 case 2:
                     displayCart(cart, scanner, totalAmount);
+                    checkOut(cart, totalAmount, scanner);
                     break;
                 case 3:
                     System.out.println("Thank you for shopping with us!");
@@ -73,6 +75,8 @@ public class Store {
         // prompt the user to enter the ID of the product they want to add to
         // their cart. The method should
         // add the selected product to the cart ArrayList.
+
+        // Prints out all products within the inventory
         System.out.println("==== Products ====");
         for (Product product : inventory) {
             System.out.println(product);
@@ -83,7 +87,7 @@ public class Store {
         id = scanner.nextLine().trim();
 
         boolean check = false;
-
+        // Looks for the ID that the user enters which is inside inventory and adds the product to cart Arraylist
         for (Product product : inventory) {
             if (product.getId().equalsIgnoreCase(id)) {
                 cart.add(product);
@@ -117,39 +121,50 @@ public class Store {
             System.out.printf("Your total will be $%.2f\n", totalAmount);
 
             String id;
-            System.out.println("Enter the id of product you would like to remove from cart");
+            System.out.println("Enter the id of product you would like to remove from cart or press enter to skip");
             id = scanner.nextLine().trim();
 
             boolean check = false;
 
-            for (Product product : cart) {
-                if (product.getId().equalsIgnoreCase(id)) {
-                    cart.remove(product);
-                    System.out.println(product.getName() + " Removed to Cart!\n");
-                    check = true;
+            if (!id.isEmpty()) {
+                // Just learned you need an iterator in order to safely remove something from a list in Java
+                Iterator<Product> iterator = cart.iterator();
+                while (iterator.hasNext()) {
+                    Product product = iterator.next();
+                    if (product.getId().equalsIgnoreCase(id)) {
+                        iterator.remove();
+                        System.out.println(product.getName() + " Removed to Cart!\n");
+                        check = true;
+                    }
                 }
+
+                if (!check) {
+                    System.err.println("No results found for product");
+                }
+
+            } else {
+                System.out.println("Skip...");
+
             }
 
-            if (!check) {
-                System.err.println("No results found for product");
-            }
         }
+
 
     }
 
-    public static void checkOut(ArrayList<Product> cart, double totalAmount) {
+    public static void checkOut(ArrayList<Product> cart, double totalAmount, Scanner scanner) {
         // This method should calculate the total cost of all items in the cart,
         // and display a summary of the purchase to the user. The method should
         // prompt the user to confirm the purchase, and calculate change and clear the cart
         // if they confirm.
-    }
-}
-/*
+
+
+
     public static Product findProductById(String id, ArrayList<Product> inventory) {
         // This method should search the inventory ArrayList for a product with
         // the specified ID, and return the corresponding com.pluralsight.Product object. If
         // no product with the specified ID is found, the method should return
         // null.
+        
     }
 }
-*/
